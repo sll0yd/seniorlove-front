397c41d8cbed4c3ce584362c342060ebb915c60b
AlfaTemplar <adrien.rimbault@gmail.com>
2024-10-29 14:28:50 +0100

    test

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Commencez dès aujourd’hui à rencontrer des personnes prêtes à partager de beaux moments et à construire une relation sincère.</p>
+      
+    </footer>
+  );
+}
+
+export default Footer;
