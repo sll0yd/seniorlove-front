edebc40b749366f30df203a64a931d6254574928
Stevens Lloyd <stevens91@gmail.com>
2024-10-29 13:53:00 +0100

    create folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./index.css";
+import Nav from "./components/Nav";
 
 function App() {
 	return (
 		<>
+			<Nav />
 			<h1>Senior Love 💘</h1>
 		</>
 	);
---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+function Nav() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<p>TEst </p>
+				</li>
+				<li>
+					<p>test</p>
+				</li>
+        <li>
+					<p>test</p>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
