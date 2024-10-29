7f9cc3e7519b1aeaf264075bff80a9f5ffa2cc74
Marc-BARBIERI <barbierimarc84@gmail.com>
2024-10-29 18:10:25 +0100

    component nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-pink": "#F2BBB7",
+        "custom-blue": "#4EC5CA",
+      },
+    },
   },
   plugins: [],
-}
-
+};
