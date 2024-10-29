eddc9671bf17cf570e3cbf0f7d28137f2f160c0c
AlfaTemplar <adrien.rimbault@gmail.com>
2024-10-29 15:36:39 +0100

    feat : create footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,13 @@ function Footer() {
     <footer>
       <p>Commencez dès aujourd’hui à rencontrer des personnes prêtes à partager de beaux moments et à construire une relation sincère.</p>
       <button>S'inscrire</button>
-      <a href="Conditions générales"></a>
-      <a href="Règles de communauté"></a>
-      <a href="Politiques de confidentialité"></a>
-      <a href="Signaler un contenu illégal"></a>
-      <p>© Senior Love</p>
+      <div>
+        <a href="/conditions-generales">Conditions générales</a>
+        <a href="/regles-de-communaute">Règles de communauté</a>
+        <a href="/politiques-confidentialite">Politiques de confidentialité</a>
+        <a href="/signaler-contenu-illegal">Signaler un contenu illégal</a>
+      </div>
+        <p>© Senior Love</p>
     </footer>
   );
 }
