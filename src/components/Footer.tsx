3e1596c13749115dbe1d2721786b6bd3faf32479
AlfaTemplar <adrien.rimbault@gmail.com>
2024-10-29 15:01:41 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,12 @@ function Footer() {
   return (
     <footer>
       <p>Commencez dès aujourd’hui à rencontrer des personnes prêtes à partager de beaux moments et à construire une relation sincère.</p>
-      
+      <button>S'inscrire</button>
+      <a href="Conditions générales"></a>
+      <a href="Règles de communauté"></a>
+      <a href="Politiques de confidentialité"></a>
+      <a href="Signaler un contenu illégal"></a>
+      <p>© Senior Love</p>
     </footer>
   );
 }
