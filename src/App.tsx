478573bc086e972133883d82498e464f6d04b612
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2024-11-01 15:39:05 +0100

    feat: calling all components necessary for the homepage on the pages Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import './index.css';
-import Nav from './components/Nav';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import DiscoverSeniorLove from './components/DiscoverSeniorLove';
-import EventsForYou from './components/EventsForYou';
-import Testimony from './components/Testimony';
-import CategoriesForYou from './components/CategoriesForYou';
-import FooterBanner from './components/FooterBanner';
+import './pages/Homepage';
+import HomePage from './pages/Homepage';
 
 function App() {
   return (
     <div>
-      <Nav />
-      <Header />
-      <DiscoverSeniorLove />
-      <EventsForYou />
-      <CategoriesForYou />
-      <Testimony />
-      <FooterBanner />
-      <Footer />
+      <HomePage />
     </div>
   );
 }
