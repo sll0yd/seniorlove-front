0701b2f7b4f51dad50a06af35e2cd7d8abd5bde5
Alexandre Sampaio <alexandre.sampaio137@gmail.com>
2024-11-18 11:25:41 +0100

    feat: adding call API for testimonies

--- src/components/Testimony.tsx
+++ src/components/Testimony.tsx
@@ -1,29 +1,20 @@
 import { useEffect, useState } from 'react';
 import type { ITestimony } from '../@types';
+import AxiosInstance from '../utils/axios';
 
 function Testimony() {
   const [testimonies, setTestimonies] = useState<ITestimony[]>([]);
-
   useEffect(() => {
-    fetch('/data.json')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Erreur lors du chargement du fichier JSON');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data && Array.isArray(data.testimonies)) {
-          setTestimonies(data.testimonies);
-        } else {
-          console.error(
-            'Les données JSON ne contiennent pas un tableau de témoignages',
-          );
-        }
-      })
-      .catch((error) =>
-        console.error('Erreur lors du chargement des témoignages :', error),
-      );
+    const fetchTestimonies = async () => {
+      try {
+        const response = await AxiosInstance.get<ITestimony[]>('/testimonies');
+        setTestimonies(response.data.slice(0, 3)); // Display only the first 3 testimonies with
+        console.log('Testimonies:', response.data.slice(0, 3));
+      } catch (error) {
+        console.error('Error fetching testimonies:', error);
+      }
+    };
+    fetchTestimonies();
   }, []);
 
   return (
@@ -41,7 +32,7 @@ function Testimony() {
         <h2 className="text-2xl md:text-3xl font-bold text-center text-white mb-8 md:mb-16">
           Témoignages
         </h2>
-        
+
         {/* Container for testimonials */}
         <div className="max-w-7xl mx-auto px-2 md:px-8">
           {/* Grid container */}
@@ -54,27 +45,39 @@ function Testimony() {
                 <h3 className="font-bold text-lg md:text-xl mb-4 md:mb-6 line-clamp-2">
                   {testimony.content}
                 </h3>
-                <p className="text-sm md:text-base text-gray-600 mb-6 md:mb-8">{testimony.user_id}</p>
-                
+
                 {/* Reviewer info */}
                 <div className="flex items-center gap-4 mb-6 md:mb-8">
-                  <div className="w-12 h-12 md:w-14 md:h-14 rounded-full bg-gray-200 flex-shrink-0" />
+                  {testimony.user.picture ? (
+                    <img
+                      src={testimony.user.picture}
+                      alt="UserPicture"
+                      className="w-12 h-12 md:w-14 md:h-14 rounded-full flex-shrink-0"
+                    />
+                  ) : (
+                    <div className="w-12 h-12 md:w-14 md:h-14 rounded-full bg-gray-200 flex-shrink-0" />
+                  )}
                   <div className="min-w-0">
-                    <p className="font-medium text-base md:text-lg truncate">Reviewer name</p>
-                    <p className="text-sm md:text-base text-gray-500">12 Septembre</p>
+                    <p className="font-medium text-base md:text-lg truncate">
+                      {testimony.user.userName}
+                    </p>
+                    <p className="text-sm md:text-base text-gray-500">
+                      {new Date(testimony.created_at).toLocaleDateString(
+                        'fr-FR',
+                        {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                        },
+                      )}
+                    </p>
                   </div>
                 </div>
-                
+
                 {/* Review text */}
                 <div className="flex-grow">
                   <p className="text-sm md:text-base text-gray-600 leading-relaxed">
-                    Contrary to popular belief, Lorem ipsum is not simply random
-                    text. It has roots in a piece of classical Latin literature from
-                    45 BC, making it over 2000 years old. Richard McClintock, a
-                    Latin professor at Hampden-Sydney College in Virginia, looked up
-                    one of the more obscure Latin words, consectetur, from a Lorem
-                    Ipsum passage, and going through the cites of the word in classical
-                    literature, discovered the undoubtable source.
+                    {testimony.content}
                   </p>
                 </div>
               </div>
